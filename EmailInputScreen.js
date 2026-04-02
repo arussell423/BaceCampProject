24f5642961e48f59d1056a80e2b2b669318760fe
arussell423 <84971771+arussell423@users.noreply.github.com>
2026-04-02 17:21:28 +0100

    Update EmailInputScreen.js to import Platform from react-native

---
+++ EmailInputScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Platform } from 'react-native';
+import { connect } from 'react-redux';
+import styles from './styles';
+
+class EmailInputScreen extends Component {
+  // Component logic goes here
+}
+
+export default connect(null)(EmailInputScreen);
